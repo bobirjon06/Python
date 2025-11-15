9659f23d1bf32d2baba2c7d473c9ac83da9d3c94
Bobirjon Axmadjonv <bahmadjonov4@gmail.com>
2025-11-15 10:19:11 +0500

    done

--- l4/classwork.py
+++ l4/classwork.py
@@ -1,1 +1,13 @@
 print("does it feel alright to not know me???")
+
+fruits = {"apple", "banana", "cherry", "apple"}
+print(type(fruits))
+fruits.update({"orange"})
+print(fruits)
+
+new_fruits = {"banana", "cherry", "orange", "mango", "pineapple"}
+print(new_fruits)
+# fruits.update(new_fruits)
+print(fruits)
+print(fruits.difference(new_fruits))
+print(fruits.intersection(new_fruits))
