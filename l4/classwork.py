561d215acfa721f69f04d587f07c9b2b4abb0582
Bobirjon Axmadjonv <bahmadjonov4@gmail.com>
2025-11-16 22:41:49 +0500

    done

--- l4/classwork.py
+++ l4/classwork.py
@@ -1,13 +1,54 @@
-print("does it feel alright to not know me???")
+# print("does it feel alright to not know me???")
+#
+# fruits = {"apple", "banana", "cherry", "apple"}
+# print(type(fruits))
+# fruits.update({"orange"})
+# print(fruits)
+#
+# new_fruits = {"banana", "cherry", "orange", "mango", "pineapple"}
+# print(new_fruits)
+# # fruits.update(new_fruits)
+# print(fruits)
+# print(fruits.difference(new_fruits))
+# print(fruits.intersection(new_fruits))
+from itertools import count
 
-fruits = {"apple", "banana", "cherry", "apple"}
-print(type(fruits))
-fruits.update({"orange"})
-print(fruits)
+#1
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+print(set1.intersection(set2))
+print(set1.symmetric_difference(set2))
+print({n for n in set1 if n%2==0})
+print(max(set1))
+print(min(set1))
+if 5 in set1:
+    print(True)
+else:
+    print(False)
 
-new_fruits = {"banana", "cherry", "orange", "mango", "pineapple"}
-print(new_fruits)
-# fruits.update(new_fruits)
+print({n for n in set1 if n>0})
+print(len(set1))
+
+#2
+set1 = {1, 2, 3}
+set2 = {3, 4, 5, 6}
+print(set1 == set2)
+
+n = 20
+set1.add(n)
+print(set1)
+print(min(set1))
+set1.discard(min(set1))
+print(set1)
+
+lst = list(set1)
+lst.sort()
+print(lst)
+print(set1.union(set2))
+word = "champagne"
+set1 = set(word)
+print(set1)
+
+fruits = {"apple", "banana", "cherry"}
+fruits.pop()
 print(fruits)
-print(fruits.difference(new_fruits))
-print(fruits.intersection(new_fruits))
--- l4/hw.py
+++ l4/hw.py
@@ -1,1 +1,119 @@
-print("Hello World")
+#1
+
+set1={1,2,3,4,5,6}
+set2={4,5,6,7,8,9}
+# print(set1.difference(set2))
+sum1 = 0
+sum2 = 0
+for i in set1:
+    if i in set.difference(set2):
+        sum1 = sum1 + i
+for i in set2:
+    if i in set2.difference(set1):
+        sum2 = sum2 + i
+print(sum2-sum1)
+
+#2
+
+set1={'olma', 'anor', 'youtube', 'instagram', 'gilos'}
+set2={'youtube', 'gilos', 'anor', 'BMW', 'Tesla', 'Nissan'}
+set3={'gilos', 'olma', 'instagram', 'Tesla', 'Nissan'}
+
+print((set1.intersection(set2)).difference(set3))
+
+#3
+
+set1={4,5,6,7,8,9}
+set2={5,6,7,10,11}
+sum1 = 0
+sum2 = 0
+for i in set1.union(set2):
+    if i in set1.difference(set2) or i in set2.difference(set1):
+        sum1 = sum1 + i
+    elif i in set1.intersection(set2):
+        sum2 = sum2 + i
+# print(sum2, sum1)
+print(sum1-sum2)
+
+#4
+
+set1={"Artel","Alif","Yandex", "Google", "Meta"}
+set2={"Google", "Apple", "Amazon", "Meta"}
+set3={"Alibaba", "Uzum", "Meta", "Google", "Amazon"}
+
+print("Umumiy: ", set1.intersection(set2).intersection(set3))
+print("Set1: ", set1.difference(set2.union(set3)))
+
+#5
+
+set1={2,3,4,5,6}
+set2={4,5,6,7,8,9}
+# sumR = 0
+# setSum = set1.symmetric_difference(set2)
+# for i in setSum:
+#     sumR = sumR+i
+# print(sumR)
+print(sum(set1.symmetric_difference(set2)))
+
+#6
+
+set1={100, 20, 45, 80, 70, 50}
+set2={30, 55, 70, 60, 32, 100}
+set3 = set()
+for i in set1.union(set2):
+    if i>=60 and i in set1.intersection(set2):
+        set3.add(i)
+print(sum(set3)/len(set3))
+
+#7
+
+print(list(set(set1).intersection(set(set2))))
+
+#8
+
+nums1 = [1, 2, 3]
+nums2 = [2, 4, 6]
+set1 = set(nums1)
+set2 = set(nums2)
+list1 = list(set1-set2)
+list2 = list(set2-set1)
+ans = [list1, list2]
+print(ans)
+
+#9
+
+nums = [1,2,2,3,1,4]
+list1 = list()
+list2 = list()
+for num in nums:
+    if num not in list1:
+        list1.append(num)
+        count = 0
+        for r in nums:
+            if r==num:
+                count += 1
+        list2.append(count)
+max_fr = 0
+for i in list2:
+    if i>max_fr:
+        max_fr = i
+total = 0
+
+for i in list2:
+    if i==max_fr:
+        total = total + i
+print(total)
+
+#10
+
+list1 = [1, 2, 4, 5, 3, 2, 23, 34, 4, 20, 5, 7, 22, 1, 6]
+set1 = set(list1)
+list1 = list(set1)
+print(list1)
+
+#11
+
+set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+set2 = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+
+print(set1-set2)
