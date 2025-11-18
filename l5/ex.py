0da3487588ad57159c2b2db184980933464e58ff
Bobirjon Axmadjonv <bahmadjonov4@gmail.com>
2025-11-18 09:21:28 +0500

    done

--- l5/ex.py
+++ l5/ex.py
@@ -1,1 +1,29 @@
-print(23455)
+#1
+
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+a=3
+b=9
+
+for i in range(a, b):
+    if i > 1:
+        isPrime = True
+        if i == 2:
+            print(i)
+        else:
+            for j in range(2, i):
+                if i%j==0:
+                    isPrime = False
+                    break
+        if isPrime:
+            print(i)
+
+
+#2
+
+n = int(input())
+count = 0
+while n%2==0:
+    count += 1
+    n = n/2
+print(count)
